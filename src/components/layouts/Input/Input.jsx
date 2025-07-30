a2e489e506f04feffb34db1d6998c3dd843e88da
Vinay Kushwah <Vinaykushwah805@gmail.com>
2025-07-30 15:34:54 +0530

    New Style

--- src/components/layouts/Input/Input.jsx
+++ src/components/layouts/Input/Input.jsx
@@ -1,44 +1,46 @@
-import React, { useState } from 'react'
-import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
+import React, {useState} from 'react'
+import {FaRegEye, FaRegEyeSlash} from "react-icons/fa6";
 
-const Input = ({ value, onChange, placeholder, label, type }) => {
-  const [showPassword, setShowPassword] = useState(false);
 
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
+const Input = ({value,onChange,placeholder,label,type}) => {
+    const [showPassword ,setShowPassword] = useState(false);
 
+    const toggleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
   return (
-    <div className="flex flex-col w-full">
-      <label className="text-[13px] text-slate-800 mb-1">{label}</label>
+    <div>
+       <label className="text-[13px] text-slate-800">{label}</label>
 
-      <div className="flex items-center border px-3 py-2 rounded w-full">
-        <input
-          type={type === 'password' ? (showPassword ? 'text' : 'password') : type}
-          placeholder={placeholder}
-          className="w-full bg-transparent outline-none text-sm"
-          value={value}
-          onChange={(e) => onChange(e)}
-        />
+       <div className="input-box">
+           <input
+           type={type == 'password' ? showPassword ? 'text' : 'password' : type}
+           placeholder={placeholder}
+           className="w-full bg-transparent outline-none"
+           value={value}
+           onChange={(e) => onChange(e)}
+           />
 
-        {type === "password" && (
-          showPassword ? (
-            <FaRegEye
-              size={18}
-              className="text-purple-600 cursor-pointer"
-              onClick={toggleShowPassword}
-            />
-          ) : (
-            <FaRegEyeSlash
-              size={18}
-              className="text-slate-400 cursor-pointer"
-              onClick={toggleShowPassword}
-            />
-          )
-        )}
-      </div>
+           {type === "password" && (
+            <>
+             {showPassword ? (
+                <FaRegEye
+                 size={22}
+                 className="text-purple-600 cursor-pointer"
+                 onClick={() => toggleShowPassword()}
+                 />
+             ) : (
+                <FaRegEyeSlash
+                 size={22}
+                 className="text-slate-400 cursor-pointer"
+                 onClick={() => toggleShowPassword()}
+                />
+             )}
+             </>  
+           )}
+       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Input;
+export default Input
--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -87,7 +87,7 @@ const SignUp = () => {
 
            <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 gap-4">
             <Input
               value={fullName}
               onChange={({ target }) => setFullName(target.value)}
